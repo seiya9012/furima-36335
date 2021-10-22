5c543098fb0707e7b1783340914c1acc1b1054bc
seiya9012 <seiya.ishizaki9012@gmail.com>
2021-10-22 22:01:00 +0900

    テストコードの修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
         validates :email, uniqueness: true
       
         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
           validates :family_name
           validates :first_name
         end
