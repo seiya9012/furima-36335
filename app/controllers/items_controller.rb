61d9646bfcc1eaef924a6811a9bd2bdd931ddefa
seiya9012 <seiya.ishizaki9012@gmail.com>
2021-11-04 21:49:41 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new,:edit, :update,]
+  before_action :move_to_root_path, only: [:edit, :update,]
   def index
     @items = Item.all.order(id: 'DESC')
   end
@@ -21,11 +22,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     
   end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
   def item_params
     params.require(:item).permit(:name, :discription, :price, :category_id, :status_id, :cost_id,
                                  :prefecture_id, :shipping_day_id,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_root_path
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+    redirect_to root_path 
+    end
   end
 end
