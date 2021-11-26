850da54683a452c630d3973c022acf1fdfda6c9f
seiya9012 <seiya.ishizaki9012@gmail.com>
2021-11-26 21:08:42 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-    resources :items ,only:[:new,:edit,:update,:create,:show,:destroy]do
+  resources :items ,only:[:new,:edit,:update,:create,:show,:destroy] do
     resources :orders, only: [:index, :create]
-    end
+  end
 
 end
